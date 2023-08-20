6c68b7e641d3e525a76cc751e38fbc9d9bc0fcd8
sayed-mudassir <sayedmuddassir68@gmail.com>
2023-08-21 03:15:28 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,9 @@ public class Main {
                     }
                     break;
                 case 5:
+                    System.out.println("current song has been removed from playlist");
+                    wasNext = false;
+                    itr.remove();
                     break;
                 case 6:
                     printsong(myPlayList);
